6c911ba80ae8dd909e5dba929c421568c7474c27
Rishiraj Singh <rishirajsingh270406@gmail.com>
2026-01-28 21:12:23 +0530

    Simplify vite config - use absolute /src alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
   },
